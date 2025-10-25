53619bc25415bbf5b4421b7d41ac8ce6aec25e82
arsenii <inbox@arudenko.work>
2025-10-25 20:40:58 +0200

    add initial language file registration

--- register.php
+++ register.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\View;
 use Leantime\Core\Events\EventDispatcher;
+use Leantime\Domain\Plugins\Services\Registration;
 use Leantime\Plugins\AuditTrail\Controllers\UiController;
 
 // UI hooks for the task page
@@ -10,3 +11,6 @@ EventDispatcher::add_event_listener('leantime.domain.tickets.*.ticketTabs', [UiC
 EventDispatcher::add_event_listener('leantime.domain.tickets.*.ticketTabsContent', [UiController::class, 'onTaskTabPanes']);
 
 View::addNamespace('AuditTrail', base_path('app/Plugins/AuditTrail/Templates'));
+
+$reg = new Registration("AuditTrail");
+$reg->registerLanguageFiles();
